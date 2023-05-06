20d24e454284a2a01686dae24e95f6d74b0d47f4
fingki junaidi <fingkiki71@gmail.com>
2023-05-06 11:16:06 +0700

    commot kelompok 3

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exports\UserExport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = auth()->user();
+        
+        return response()->json([
+            'users' => $users,
+            'status' => 200
+        ]);
+    }
+    public function export() 
+        {
+            return Excel::download(new UserExport, 'Data_user.xlsx');
+        }
+}

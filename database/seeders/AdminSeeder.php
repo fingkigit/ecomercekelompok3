696472665d2b66d2ca106548c9ae3baab1eca1f6
wilekk <132779652+wilekk@users.noreply.github.com>
2023-05-07 09:32:28 +0700

    Wilekk

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AdminSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     
     public function run()
     {
